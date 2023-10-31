c852b6f0c3bc51f2b4fc7e43ab193a7f6056ea27
IgorMushk <igor.mushketik@gmail.com>
2023-10-31 23:44:16 +0200

    Upd 4.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import {Employee, Manager} from './2'
 import {Wizard} from './3'
+import {Key, Person, House, MyHouse } from './4'
 
 // test 2.ts
 console.log('--------- test 2.ts')
@@ -17,3 +18,13 @@ const wizard = new Wizard('Merlin', 15);
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
 wizard.levelUp()
+
+// test 4.ts
+console.log('--------- test 4.ts')
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
